0e965c5f4212e63227d8e21c990f9a753e4d3b1b
Froilan Luna-Lopez <flunalopez@login-0.ib.cluster>
2021-01-21 11:57:07 -0800

    rehashed

--- bin/genSplitString.py
+++ bin/genSplitString.py
@@ -1,12 +1,24 @@
-import argparse
+#import argparse
+#
+#arg_parser = argparse.ArgumentParser()
+#arg_parser.add_argument("-s", "--split", default = 1,
+#						help = "Integer value to interpret and generate corresponding split string.")
+#args = arg_parser.parse_args()
+#
+#x = args.split
+def partSuffixes(x):
+		splitSuffix = list("000")
+		split_arr = []
 
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument("-s", "--split", default = 1,
-						help = "Integer value to interpret and generate corresponding split string.")
-args = arg_parser.parse_args()
-
-x = args.split
-splitSuffix = list("000")
-for i,j in zip(range(len(x)-1,-1,-1), range(2,-1,-1)):
-	splitSuffix[j] = x[i]
-print(".part_" + "".join(splitSuffix))
+		###
+		# @idx Range from zero to split value
+		###
+		for idx in range(1,int(x)+1):
+			###
+			# @i Range from zero to size of integer idx
+			# @j Range from zero to two for position in part mold
+			###
+			for i,j in zip(range(len(str(idx))-1,-1,-1), range(2,-1,-1)):
+				splitSuffix[j] = str(idx)[i]
+			split_arr.append("".join(splitSuffix))
+		return split_arr
