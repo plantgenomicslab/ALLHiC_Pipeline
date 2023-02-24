e1bf777205c369aa995996b5056ed24fb72a2a2a
wyim-pgl <wyim@unr.edu>
2023-02-24 14:24:34 -0800

    add chromap

--- run.sh
+++ run.sh
@@ -3,11 +3,17 @@
 set -x
 set -e
 
-snakemake --rerun-incomplete --cluster-config cluster.json \
-          --cluster "sbatch -N {cluster.nodes} --mem={cluster.memory} --cpus-per-task={cluster.ncpus} --parsable -A {cluster.account} -p {cluster.partition} -t {cluster.time} -o {cluster.output} -e {cluster.error}" \
+snakemake --rerun-incomplete \
+          --printshellcmds \
           --max-jobs-per-second 5 \
           --max-status-checks-per-second 3 \
-		  --jobs 4 \
-		  --latency-wait 40 \
-		  --notemp \
+          --jobs 48 \
+          --latency-wait 40 \
+          --notemp \
+          --cluster "sbatch -N {cluster.nodes} --mem={cluster.memory} --cpus-per-task={cluster.ncpus} --parsable -A {cluster.account} -p {cluster.partition} -t {cluster.time} -o {cluster.output} -e {cluster.error}" \
+          --cluster-config cluster.json \
+          --cluster-status scripts/status.py \
+          --cluster-cancel 'scancel' \
           "$@"
+
+
